b50fba01e4fcb2cd6b1608cf8b4d190f5a24b3c5
Zhipeng-Xu <91949379+Zhipeng-Xu@users.noreply.github.com>
2024-02-19 21:11:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,67 @@
+# ActiveRAG
+
+Source code for our paper :
+***ActiveRAG: Revealing the Treasures of Knowledge via Active Learning***
+
+If you find this work useful, please cite our paper  and give us a shining star 🌟
+
+## Quick Start
+
+### Install from git
+
+```bash
+git clone https://github.com/OpenMatch/ActiveRAG
+pip install -r requirements.txt
+```
+
+### Reproduction
+
+We provide our request logs, so the results in the paper can be quickly reproduced:
+
+```bash
+python -m logs.eval --dataset nq --topk 5
+```
+
+**Parameters:**
+
+- `dataset`: dataset name.
+- `topk`: using top-k of retrieved results to augment.
+
+##  Re-request
+
+We also provide the full request code, you can re-request for further exploration.
+
+First, set your own api-key in agent file:
+
+```python
+openai.api_key = 'sk-<your-api-key>'
+```
+
+Then, run the following script:
+
+```bash
+python -m scripts.run --dataset nq --topk 5
+```
+
+Analyzing log files:
+
+```bash
+python -m scripts.build --dataset nq --topk 5
+```
+
+Evaluate:
+
+```bash
+python -m scripts.evaluate --dataset nq --topk 5
+```
+
+
+
+## Contact Us
+
+If you have questions, suggestions, and bug reports, please send a email to us, we will try our best to help you. 
+
+```bash
+xuzhipeng@stumail.neu.edu.cn  
+```
+
---
+++ scripts/run.py
@@ -0,0 +1,54 @@
+from src.generate import create_agent_group
+from src.generate import create_plan
+from argparse import ArgumentParser
+from src.prompt import Prompt
+from datetime import datetime
+from tqdm import tqdm
+import traceback
+import time
+import json
+import os
+
+parser = ArgumentParser()
+parser.add_argument('--dataset', required=True)
+parser.add_argument('--topk', type=int, required=True)
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    dataset = args.dataset
+    filename = f'data/data_{dataset}_sampled.jsonl'
+    topk = args.topk
+
+    directory = f'log/{dataset}/top{topk}'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        for i, line in tqdm(enumerate(file)):
+            try:
+                data = json.loads(line)
+                id = data['id']
+                question = data['question']
+                answers = data['answers']
+                passages = data['passages'][:topk]
+
+
+                input = {
+                    'question':question,
+                    'passages':passages,
+                    '__answers__':answers,
+                }
+
+                print(f"{i}. {question}")
+                time.sleep(5)
+                plan = create_plan(create_agent_group(Prompt()),init_input=input)
+                plan.excute()
+                
+                plan.save_log(f'log/{dataset}/top{topk}/{dataset}_idx_{i}.json')
+
+            except Exception as e:
+                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f"Error at index {i} - {current_time}: {e}")
+                traceback.print_exc()
+                break
